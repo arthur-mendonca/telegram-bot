3373eb8fc991396ef570c534856d24971e142923
Arthur Mendonça <arthu91@gmail.com>
2025-02-02 18:42:16 -0300

    format btc endpoint

--- controller/lib/Telegram.js
+++ controller/lib/Telegram.js
@@ -1,4 +1,10 @@
-const { axiosInstance } = require("./axios");
+const { coinGeckoAxiosInstance, telegramAxiosInstance } = require("./axios");
+
+const formatCurrency = (value, locale, currency) =>
+  value.toLocaleString(locale, {
+    style: "currency",
+    currency,
+  });
 
 const sendMessage = (messageObj, messageText) => {
   try {
@@ -6,7 +12,7 @@ const sendMessage = (messageObj, messageText) => {
       console.error("Invalid message object:", messageObj);
       return Promise.reject(new Error("Invalid message object"));
     }
-    return axiosInstance.get("sendMessage", {
+    return telegramAxiosInstance.get("sendMessage", {
       chat_id: messageObj.chat.id,
       text: messageText,
     });
@@ -23,7 +29,7 @@ const sendMessage = (messageObj, messageText) => {
   }
 };
 
-const handleMessages = (messageObj) => {
+const handleMessages = async (messageObj) => {
   if (!messageObj || typeof messageObj !== "object" || !messageObj.chat) {
     console.error("Invalid message object:", messageObj);
     return Promise.reject(new Error("Invalid message object"));
@@ -37,15 +43,63 @@ const handleMessages = (messageObj) => {
       case "start":
         return sendMessage(
           messageObj,
-          "Hello, welcome to my bot. How can I help you?"
+          "Olá, bem-vindo ao Cripto-bot, digite /btc para saber o preço do Bitcoin"
         );
       case "help":
-        return sendMessage(messageObj, "Type /start to start the bot");
+        return sendMessage(messageObj, "Digite /start para iniciar");
+      case "btc":
+        try {
+          const response = await coinGeckoAxiosInstance.get("simple/price", {
+            ids: "bitcoin",
+            vs_currencies:
+              "brl,usd,eur,gbp,cad,aud,cny,jpy,ars,mxn,rub,inr,sats",
+          });
+
+          const {
+            brl,
+            usd,
+            eur,
+            gbp,
+            cad,
+            aud,
+            cny,
+            jpy,
+            ars,
+            mxn,
+            rub,
+            inr,
+            sats,
+          } = response.data.bitcoin;
+
+          const priceMessage = `💰 Preço Atual do Bitcoin:\n\n
+          🇧🇷 BRL: ${formatCurrency(brl, "pt-BR", "BRL")}
+          🇺🇸 USD: ${formatCurrency(usd, "en-US", "USD")}
+          🇪🇺 EUR: ${formatCurrency(eur, "de-DE", "EUR")}
+          🇬🇧 GBP: ${formatCurrency(gbp, "en-GB", "GBP")}
+          🇨🇦 CAD: ${formatCurrency(cad, "en-CA", "CAD")}
+          🇦🇺 AUD: ${formatCurrency(aud, "en-AU", "AUD")}
+          🇨🇳 CNY: ${formatCurrency(cny, "zh-CN", "CNY")}
+          🇯🇵 JPY: ${formatCurrency(jpy, "ja-JP", "JPY")}
+          🇦🇷 ARS: ${formatCurrency(ars, "es-AR", "ARS")}
+          🇲🇽 MXN: ${formatCurrency(mxn, "es-MX", "MXN")}
+          🇷🇺 RUB: ${formatCurrency(rub, "ru-RU", "RUB")}
+          🇮🇳 INR: ${formatCurrency(inr, "hi-IN", "INR")}
+          🟠 Satoshis: ${sats.toLocaleString()} sats`;
+
+          return sendMessage(messageObj, priceMessage);
+        } catch (error) {
+          console.error("Erro ao buscar preço do BTC:", error);
+          return sendMessage(
+            messageObj,
+            "⚠️ Erro ao obter o preço do Bitcoin. Tente novamente mais tarde."
+          );
+        }
+
       default:
-        return sendMessage(messageObj, "I don't understand");
+        return sendMessage(messageObj, "Desculpa, não entendo este comando.");
     }
   } else {
-    return sendMessage(messageObj, "I don't understand");
+    return sendMessage(messageObj, "Desculpa, não entendo este comando.");
   }
 };
 
